f82c3ddf01793226cdec57979167ccb961a1f2da
mromagne <michael.romagne@gmail.com>
2023-09-20 18:20:00 +0200

    about me

--- js/index.js
+++ js/index.js
@@ -13,4 +13,19 @@ navLinks.forEach(link => {
     })
 })
 
+const aboutMeContainer = document.getElementById('about-me-content');
 
+fetch('about_me.txt')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then((aboutMeText) => {
+    aboutMeText = aboutMeText.replace(/\n/g, '<br>');
+    aboutMeContainer.innerHTML = aboutMeText;
+  })
+  .catch((error) => {
+    console.error('Error fetching About Me content:', error);
+  });
