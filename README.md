2de87d4b84fd5e9308bb8612853ceb86063f42e5
mromagne <michael.romagne@gmail.com>
2023-09-21 16:27:46 +0200

    fix tags

--- README.md
+++ README.md
@@ -2,20 +2,12 @@
 
 My Data Science portfolio website.
 
-Because I am not a web developer and do not have natural web design talents, I took inspirations from a few places : 
+Inspiration taken from : 
 - Kevin Powell [course on Scrimba](https://scrimba.com/learn/portfolio)
+- https://css-tricks.com/snippets/css/typewriter-effect/
+- https://tkharrat.info/ 
+
 
 Logo from [flaticon](https://www.flaticon.com/fr/)
 
-# TODO
 
-- Adapt fonts
-- adapt colors
-- change images, maybe generated stuff
-- find cool stuff on templates HTML, CSS, JS to make my own site really
-- Share about cool libraries I use in my day to day work
-- Share dfg project, football with polars, maybe aoc
-
-Wanted theme :
-
-- Clear theme, white background, probably marine blue as color somewhere ?
--- js/index.js
+++ js/index.js
@@ -43,25 +43,20 @@ tagFilterButtons.forEach((button) => {
     button.addEventListener('click', () => {
         const selectedTag = button.getAttribute('data-tag');
 
-        // Toggle the active class on the clicked button
         button.classList.toggle('active');
 
-        // Hide all portfolio items
         portfolioItems.forEach((item) => {
             item.style.display = 'none';
         });
 
-        // Create an array to store active tags
         const activeTags = [];
 
-        // Collect all active tags
         tagFilterButtons.forEach((btn) => {
           if (btn.classList.contains('active')) {
               activeTags.push(btn.getAttribute('data-tag'));
           }
         });
 
-        // Show portfolio items with the selected tag
         portfolioItems.forEach((item) => {
             const tags = item.getAttribute('data-tags').split(',');
             if (activeTags.length === 0 || activeTags.every(tag => tags.includes(tag))) {
@@ -71,17 +66,75 @@ tagFilterButtons.forEach((button) => {
     });
 });
 
-// Show All
+// Show All button
 const showAllButton = document.querySelector('.show-all-button');
 
 showAllButton.addEventListener('click', () => {
-  // Remove the 'active' class from all filter buttons
   tagFilterButtons.forEach((button) => {
     button.classList.remove('active');
   });
 
-  // Show all portfolio items
   portfolioItems.forEach( (item) => {
     item.style.display = 'block';
   });
 });
+
+
+
+// Typewriting effect
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  document.body.appendChild(css);
+};
