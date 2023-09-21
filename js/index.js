a45be8b4c2d591cacc2d4df46684f111c0beb990
mromagne <michael.romagne@gmail.com>
2023-09-21 11:07:24 +0200

    first idea of tags & filters

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+// Navigation button
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
@@ -13,6 +15,8 @@ navLinks.forEach(link => {
     })
 })
 
+
+// Include about_me.txt file in the About Me section
 const aboutMeContainer = document.getElementById('about-me-content');
 
 fetch('about_me.txt')
@@ -29,3 +33,55 @@ fetch('about_me.txt')
   .catch((error) => {
     console.error('Error fetching About Me content:', error);
   });
+
+
+// Tag filtering for portfolio items
+const tagFilterButtons = document.querySelectorAll('.tag-filter');
+const portfolioItems = document.querySelectorAll('.portfolio__item');
+
+tagFilterButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const selectedTag = button.getAttribute('data-tag');
+
+        // Toggle the active class on the clicked button
+        button.classList.toggle('active');
+
+        // Hide all portfolio items
+        portfolioItems.forEach((item) => {
+            item.style.display = 'none';
+        });
+
+        // Create an array to store active tags
+        const activeTags = [];
+
+        // Collect all active tags
+        tagFilterButtons.forEach((btn) => {
+          if (btn.classList.contains('active')) {
+              activeTags.push(btn.getAttribute('data-tag'));
+          }
+        });
+
+        // Show portfolio items with the selected tag
+        portfolioItems.forEach((item) => {
+            const tags = item.getAttribute('data-tags').split(',');
+            if (activeTags.length === 0 || activeTags.every(tag => tags.includes(tag))) {
+              item.style.display = 'block';
+            }
+        });
+    });
+});
+
+// Show All
+const showAllButton = document.querySelector('.show-all-button');
+
+showAllButton.addEventListener('click', () => {
+  // Remove the 'active' class from all filter buttons
+  tagFilterButtons.forEach((button) => {
+    button.classList.remove('active');
+  });
+
+  // Show all portfolio items
+  portfolioItems.forEach( (item) => {
+    item.style.display = 'block';
+  });
+});
