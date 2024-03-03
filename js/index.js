022121e443477e7ae93ab6483b7c7f59f82e3eaf
Michaël Romagné <michael.romagne@gitguardian.com>
2024-03-03 17:08:07 +0100

    fix: javascript error

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
 
-// Navigation button
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-
-navToggle.addEventListener('click', ()=> {
-    document.body.classList.toggle('nav-open');
-});
-
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
-
-
 // Include about_me.txt file in the About Me section
 const aboutMeContainer = document.getElementById('about-me-content');
 
